d9048282cba73679dcb35c0a238b8ef57f7e29a0
Gharbi Mohammed <simo163@hotmail.co.uk>
2016-04-30 20:12:43 +0200

    fixing all specs according to welcome_ui

--- web_container/config/routes.rb
+++ web_container/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  mount AppComponent::Engine, at: '/app_component'
   mount GamesAdmin::Engine, at: '/games_admin'
   mount TeamsAdmin::Engine, at: '/teams_admin'
   mount PredictorUi::Engine, at: '/predictor_ui'
+  mount WelcomeUi::Engine, at: '/welcome_ui'
 
-  root to: 'app_component/welcome#index'
+  root to: 'welcome_ui/welcome#index'
 end
